c14bf1eabab0e673693e6b5898c66058aaacea26
Cicero Bruno <cbruno.dev@gmail.com>
2025-04-24 15:14:44 -0300

    Update: main window layout

--- main.py
+++ main.py
@@ -2,13 +2,30 @@ import sys
 
 from PySide6.QtWidgets import QApplication
 
+from utils.assets_path import resource_path
 from views.main_window import MainWindow
+
+
+def load_stylesheet(file_path: str) -> str:
+    path = resource_path(file_path)
+    with open(path, "r") as file:
+        return file.read()
+
+
+def center_window(window) -> None:
+    screen = QApplication.primaryScreen()
+    screen_geometry = screen.availableGeometry()
+    screen_center = screen_geometry.center()
+    window_geometry = window.frameGeometry()
+    window_geometry.moveCenter(screen_center)
+    window.move(window_geometry.topLeft())
 
 
 def main():
     app = QApplication()
-
+    app.setStyleSheet(load_stylesheet("assets/style.qss"))
     window = MainWindow()
+    center_window(window)
     window.show()
 
     sys.exit(app.exec())
--- views/main_window.py
+++ views/main_window.py
@@ -1,9 +1,57 @@
-from PySide6.QtCore import QSize
-from PySide6.QtWidgets import QWidget
+from PySide6.QtCore import Qt, QSize
+from PySide6.QtGui import QIcon, QPixmap, QIntValidator
+from PySide6.QtWidgets import QWidget, QLineEdit, QVBoxLayout, QLabel, QPushButton, QProgressBar, \
+    QGridLayout
+
+from utils.assets_path import resource_path
 
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("RRC Tool")
-        self.setFixedSize(QSize(400,400))
+        self.setWindowTitle("RRC Tool - ASSTEC")
+        self.setFixedSize(QSize(400, 400))
+
+        # Components
+        self.logo = QLabel()
+        self.logo.setPixmap(QPixmap(resource_path("assets/logo.png")))
+        self.logo.setScaledContents(True)
+        self.logo.setFixedSize(QSize(300, 200))
+
+        self.progress_bar = QProgressBar()
+        self.progress_bar.setRange(0, 0)
+        self.progress_bar.setFixedHeight(10)
+        self.progress_bar.setTextVisible(False)
+        self.progress_bar.setVisible(False)
+
+        self.asstec_search_bar = QLineEdit()
+        self.asstec_search_bar.setValidator(QIntValidator())
+        self.asstec_search_bar.setPlaceholderText("Nº da ASSTEC")
+
+        self.status_label = QLabel("Placeholder...")
+        self.status_label.setObjectName("status_label")
+
+        self.generate_document_button = QPushButton(QIcon(resource_path("assets/icons/document-add.svg")), " Gerar RRC")
+        self.generate_document_button.setIconSize(QSize(30, 30))
+
+        self._setup_layout()
+
+    def _setup_layout(self):
+        main_layout = QVBoxLayout(self)
+        main_layout.setAlignment(Qt.AlignmentFlag.AlignHCenter | Qt.AlignmentFlag.AlignTop)
+
+        progress_widget = QWidget()
+        progress_widget.setFixedHeight(10)
+        progress_widget_layout = QVBoxLayout(progress_widget)
+        progress_widget_layout.addWidget(self.progress_bar)
+        progress_widget_layout.setContentsMargins(0, 0, 0, 0)
+
+        g_layout = QGridLayout()
+        g_layout.addWidget(self.asstec_search_bar, 0, 0, 1, 4)
+        g_layout.addWidget(progress_widget, 1, 0, 1, 4)
+        g_layout.addWidget(self.generate_document_button, 2, 1, 1, 2)
+        g_layout.addWidget(self.status_label, 3, 0, 1, 4)
+
+        main_layout.addWidget(self.logo)
+        main_layout.addSpacing(10)
+        main_layout.addLayout(g_layout)
